a33116c72e536a76846b5f7f24cd207c9a044d8c
feifanuoft <ffan.li@mail.utoronto.ca>
2024-09-30 17:35:30 -0400

    add project

---
+++ src/App.tsx
@@ -0,0 +1,79 @@
+import { Authenticated, GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import { useNotificationProvider } from "@refinedev/antd";
+import "@refinedev/antd/dist/reset.css";
+
+import { authProvider, dataProvider,liveProvider } from "./providers";
+import {Home, ForgotPassword, Login, Register} from "./pages";
+
+import routerBindings, {
+  CatchAllNavigate,
+  DocumentTitleHandler,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router-v6";
+
+import { App as AntdApp, ConfigProvider } from "antd";
+import Layout from "./components/layout";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import { resources } from "./config/resources";
+import CompanyList from "./pages/company/list";
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <RefineKbarProvider>
+          <AntdApp>
+            <DevtoolsProvider>
+              <Refine
+                dataProvider={dataProvider}
+                liveProvider={liveProvider}
+                notificationProvider={useNotificationProvider}
+                routerProvider={routerBindings}
+                authProvider={authProvider}
+                resources={resources}
+                options={{
+                  syncWithLocation: true,
+                  warnWhenUnsavedChanges: true,
+                  useNewQueryKeys: true,
+                  projectId: "H6dYDo-Ia2tUt-mmM7g0",
+                  liveMode: "auto",
+                }}
+              >
+                <Routes>
+                  <Route path = "/register" element={<Register />} />
+                  <Route path = "/login" element={<Login />} />
+                  <Route path = "/forgotPassword" element={<ForgotPassword />} />
+                  <Route
+                  element = {
+                  <Authenticated 
+                  key = "authenticated-layout"
+                  fallback = {<CatchAllNavigate to="/login" />}
+                  >
+                    <Layout>
+                      <Outlet />
+                    </Layout>
+
+                  </Authenticated>
+                  }>
+                    <Route index element={<Home />} />
+                    <Route path = "/companies" element={<CompanyList />} />
+                  </Route>
+                </Routes>
+                <RefineKbar />
+                <UnsavedChangesNotifier />
+                <DocumentTitleHandler />
+              </Refine>
+              <DevtoolsPanel />
+            </DevtoolsProvider>
+          </AntdApp>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
